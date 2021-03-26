0242a462dcd9397cde4a513e41bf1a63b162bbb0
Lora K <lora.kalthoff@gmail.com>
2021-03-26 14:41:44 -0600

    MongoDB Classwork

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # webapp_2021_confetti_cuisine
-## Node.js with Express Classwork
+## MongoDB Classwork
 - Lora Kalthoff
 - WebAppDev CSCI 4800
+
+To View:
++ Download zip from github and unzip folder
++ In command prompt navigate to directory and type:
+    >npm start
++ Then in web browser, go to this address:
+    >http://localhost:3000/
++ To view list of subscribers:
+    >http://localhost:3000/subscribers
 
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const express = require("express"), app = express(),
-homeController = require("./controllers/homeController"),
-errorController = require("./controllers/errorController"),
-layouts = require("express-ejs-layouts");
+    homeController = require("./controllers/homeController"),
+    errorController = require("./controllers/errorController"),
+    subscribersController = require("./controllers/subscribersController"),
+    layouts = require("express-ejs-layouts"), mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/confetti_cuisine",
+    { useNewUrlParser: true }); //if database doesn't exist, will be created
 
 app.set("port", process.env.PORT || 3000);
 
@@ -21,8 +25,11 @@ app.use(express.json());
 
 //routes
 app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSignUp);
-app.post("/contact", homeController.postedSignUpForm); //could be sent back to same route
+// app.get("/contact", homeController.showSignUp);
+// app.post("/contact", homeController.postedSignUpForm); //could be sent back to same route
+app.get("/subscribers", subscribersController.getAllSubscribers);
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
 
 //error handling
 app.use(errorController.pageNotFoundError);
